d79f275c31797f5bedb648311191fda68b64baa5
tanaka <tanaka.mawoyo@yahoo.com>
2022-12-29 14:42:52 +0100

    Swifter Framework Added

--- TwitterSentiment/ViewController.swift
+++ TwitterSentiment/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier  = TweetSentimentClassifier()
+    
+    
+    let swifter = Swifter(consumerKey: "3to6tX5wYylR9SdZEfELHUDRK", consumerSecret: "d6OXwc9buSqngWWD8ej38FTxO7VFs89agNi9dFjj17Ck5eERpN")
+    
+    
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+           // Do any additional setup after loading the view.
+        
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a great company")
+        
+        print(prediction.label)
+        
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended ) { (results, metadata) in
+           // print(results)
+        } failure: { error in
+            print("there was an error with the Twitter API Request, \(error)")
+        }
+        
+
+       }
+    
+    @IBAction func predictPressed(_ sender: Any) {
+        
+        
     }
-
-
+    
 }
 
